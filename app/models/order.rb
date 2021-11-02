8d13d93b04672c1a32417c00acd2c04e1f7bd7b9
Arun Tekwani <atekwani@deqode.com>
2021-11-02 13:03:49 +0530

    add qty correction callback

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,14 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :order_lines, dependent: :destroy
   accepts_nested_attributes_for :order_lines, allow_destroy: true
+  after_create :destroy_cart
 
   enum status: ["pending", "confirmed", "cancelled", "completed"]
+
+  private
+
+  def destroy_cart
+    user.carts.where(product_id: order_lines.pluck(:product_id)).destroy_all
+  end
+
 end
--- app/models/order_line.rb
+++ app/models/order_line.rb
@@ -1,6 +1,21 @@
 class OrderLine < ApplicationRecord
   belongs_to :order
   belongs_to :product
+  validate :check_product_qty
+  after_create :correct_product_qty
 
   enum status: ["pending", "confirmed", "cancelled", "completed"]
+
+  private
+
+  def check_product_qty
+    if qty > product.stock_qty 
+      errors.add(:base, "#{product.name} is out of stock")
+    end 
+  end
+
+  def correct_product_qty
+    product.update(stock_qty: product.stock_qty-qty)
+  end
+
 end
