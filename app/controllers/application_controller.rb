dee134ff5c99f56665689b6b89ca6f125a9f5cf8
Arun Tekwani <atekwani@deqode.com>
2021-11-16 20:08:09 +0530

    Add user decorator

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
+	def current_user
+    UserDecorator.decorate(super) unless super.nil?
+  end
+
  	protected
 
 	def configure_permitted_parameters
---
+++ app/decorators/user_decorator.rb
@@ -0,0 +1,21 @@
+class UserDecorator < Draper::Decorator
+  delegate_all
+
+  def full_name
+    if first_name.blank? && last_name.blank?
+      'No name provided.'
+    else
+      "#{ first_name } #{ last_name }".strip
+    end
+  end
+
+  # Define presentation-specific methods here. Helpers are accessed through
+  # `helpers` (aka `h`). You can override attributes, for example:
+  #
+  #   def created_at
+  #     helpers.content_tag :span, class: 'time' do
+  #       object.created_at.strftime("%a %m/%d/%y")
+  #     end
+  #   end
+
+end
